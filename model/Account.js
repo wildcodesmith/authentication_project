a92cfccf366ad1487a29557b5ba25f853823d63b
wildcodesmith <sumit113520@gmail.com>
2026-07-07 11:00:52 +0530

    files modified

--- model/Account.js
+++ model/Account.js
@@ -6,12 +6,12 @@ const accountSchema = new mongoose.Schema({
     email: { type: String, required: true },
     password: { type: String, required: true },
     
-    // 1. NEW: Add the verification flag and creation timestamp
+    //verification flag and creation timestamp
     isVerified: { type: Boolean, default: false },
     createdAt: { type: Date, default: Date.now } 
 });
 
-// 2. NEW: Tell MongoDB to auto-delete unverified accounts after 15 minutes (900 seconds)
+//  auto-delete unverified accounts after 15 minutes (900 seconds)
 accountSchema.index(
     { createdAt: 1 }, 
     { 
